67a80d24a3b39d8b9cfeb9bc7d9cb3bcbe215ac3
Cotze <carlosrodigoatl@gmail.com>
2021-12-28 14:48:44 -0600

    Creacion del componete dbz y manejo de estructura

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Personaje {
+  nombre: string,
+  poder: number
+}
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html'
+})
+export class MainPageComponent {
+
+//Tarea
+//Cambiar los li del html por los pesenajes del srray
+
+  personajes: Personaje[] = [
+  {
+    nombre: 'Goku',
+    poder: 15000
+  },
+  {
+    nombre: 'Vegeta',
+    poder: 8500
+  }
+];
+
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  };
+
+  agregar(  ) {
+    if ( this.nuevo.nombre.trim().length === 0 ){
+      return;
+    }
+    console.log( this.nuevo );
+    
+    this.personajes.push( this.nuevo );
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
+  }
+
+}
