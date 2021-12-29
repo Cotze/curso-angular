fb760665833e3ba63655e4fd239355f975fcb10f
Cotze <carlosrodigoatl@gmail.com>
2021-12-29 16:43:39 -0600

    Primer metodo en el service

--- src/app/dbz/agregar/agregar.component.ts
+++ src/app/dbz/agregar/agregar.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Personaje } from '../intarfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-agregar',
@@ -7,23 +8,25 @@ import { Personaje } from '../intarfaces/dbz.interface';
 })
 export class AgregarComponent {
 
-  
-
   @Input()
   nuevo: Personaje = {
     nombre: '',
     poder: 0
   };
 
+  constructor( private DbzService: DbzService){
+
+  }
+
   //@Output() sirve para emitir eventos
-  @Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
+  //@Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
   agregar(  ) {
     if ( this.nuevo.nombre.trim().length === 0 ){
       return;
     }
     //con emit se emite el evento del @Output
-    this.onNuevoPersonaje.emit( this.nuevo );
-
+    //this.onNuevoPersonaje.emit( this.nuevo );
+    this.DbzService.agregarPersonaje( this.nuevo );
     this.nuevo = {
       nombre: '',
       poder: 0
--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -19,8 +19,9 @@ export class DbzService {
         return [...this._personajes];
     }
 
-    constructor(){
-        console.log( 'servicio incializado' );
-        
+    constructor(){}
+
+    agregarPersonaje( personaje:Personaje ){
+      this._personajes.push( personaje );
     }
 }
